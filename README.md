a389e7a8beec3eee68e78a2d45b2cfebad823704
Desire Richards-Campbell <51263399+DesireCampbell@users.noreply.github.com>
2026-02-20 09:18:19 -0500

    stripstyles temp function works for selection or whole doc

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # fao-html-scripts README
 
 A small collectin of scripts to help in HTML production. A work in progress.
+
+[https://github.com/DesireCampbell/fao-html-scripts](https://github.com/DesireCampbell/fao-html-scripts)
--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,46 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "fao-html-scripts" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('fao-html-scripts.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	const helloWorld = vscode.commands.registerCommand('fao-html-scripts.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from FAO HTML Scripts!');
 	});
+	context.subscriptions.push(helloWorld);
 
-	context.subscriptions.push(disposable);
+	const stripStyles = vscode.commands.registerCommand('fao-html-scripts.stripStyles', () => {
+		vscode.window.showInformationMessage('FAO Strip Styles');
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor;
+		
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			if (selection.isEmpty) {
+				// If no text is selected, replace entire document with reversed content
+				const fullText = document.getText();
+				const reversed = fullText.split('').reverse().join('');
+				const firstLine = document.lineAt(0);
+				const lastLine = document.lineAt(document.lineCount - 1);
+				const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+				editor.edit(editBuilder => {
+					editBuilder.replace(textRange, reversed);
+				});
+			}else {
+				// Get the word within the selection
+				const word = document.getText(selection);
+				const reversed = word.split('').reverse().join('');
+				editor.edit(editBuilder => {
+					editBuilder.replace(selection, reversed);
+				});
+			}
+
+		}
+
+	});
+	context.subscriptions.push(stripStyles);
+
 }
 
 // This method is called when your extension is deactivated
