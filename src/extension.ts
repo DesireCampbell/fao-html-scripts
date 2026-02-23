d0b162e5cc21ecbb998698c8df66145f0ac3d8c4
Desire Richards-Campbell <51263399+DesireCampbell@users.noreply.github.com>
2026-02-23 10:08:14 -0500

    getter and setter helper functions

--- src/extension.ts
+++ src/extension.ts
@@ -7,41 +7,65 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "fao-html-scripts" is now active!');
 
+	
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	// Get Text in Selection or Document
+	const getCurrentSelectionOrDocumentText = () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) { return; }
+		const document = editor.document;
+		const selection = editor.selection;
+		if (selection.isEmpty) {
+			return document.getText();
+		}else {
+			return document.getText(selection);
+		}
+	};
+			
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	// Replace Text in Selection or Document
+	const replaceCurrentSelectionOrDocumentText = (textOut: string) => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) { return; }
+		const document = editor.document;
+		const selection = editor.selection;
+		if (selection.isEmpty) {
+			const firstLine = document.lineAt(0);
+			const lastLine = document.lineAt(document.lineCount - 1);
+			const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+			editor.edit(editBuilder => { editBuilder.replace(textRange, textOut); });
+		}else {
+			editor.edit(editBuilder => { editBuilder.replace(selection, textOut); });
+		}
+	};
+
+
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	// Hello World
 	const helloWorld = vscode.commands.registerCommand('fao-html-scripts.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from FAO HTML Scripts!');
 	});
 	context.subscriptions.push(helloWorld);
 
+
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	// Strip Styles
 	const stripStyles = vscode.commands.registerCommand('fao-html-scripts.stripStyles', () => {
-		vscode.window.showInformationMessage('FAO Strip Styles');
+		vscode.window.showInformationMessage('FAO Strip Styles');		
 		// Get the active text editor
 		const editor = vscode.window.activeTextEditor;
-		
-		if (editor) {
-			const document = editor.document;
-			const selection = editor.selection;
+		// If there's no active editor, do nothing
+		if (!editor) { return; }
 
-			if (selection.isEmpty) {
-				// If no text is selected, replace entire document with reversed content
-				const fullText = document.getText();
-				const reversed = fullText.split('').reverse().join('');
-				const firstLine = document.lineAt(0);
-				const lastLine = document.lineAt(document.lineCount - 1);
-				const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-				editor.edit(editBuilder => {
-					editBuilder.replace(textRange, reversed);
-				});
-			}else {
-				// Get the word within the selection
-				const word = document.getText(selection);
-				const reversed = word.split('').reverse().join('');
-				editor.edit(editBuilder => {
-					editBuilder.replace(selection, reversed);
-				});
-			}
+		// get text
+		let textIn = getCurrentSelectionOrDocumentText() || '';
+		let textOut = '';
 
-		}
+		// run the script
+		textOut = textIn.split('').reverse().join('');
 
+		// replace text
+		replaceCurrentSelectionOrDocumentText(textOut);
 	});
 	context.subscriptions.push(stripStyles);
 
