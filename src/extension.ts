69fbf6ecfad06967dfeca32f0c9db81f5ab1aedd
Desire Richards-Campbell <51263399+DesireCampbell@users.noreply.github.com>
2026-09-17 13:44:02 -0400

    working

--- src/extension.ts
+++ src/extension.ts
@@ -1891,7 +1891,6 @@ function getClassedTableHtml(table:string = '') {
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   // Copy Paste Table Styles
-  let tableStylesClipboard:any[] = [];
   const copyTableStyles = vscode.commands.registerCommand('fao-html-scripts.copyTableStyles', () => {
     faodebug.appendLine('SCRIPT: copyTableStyles');
     let numFound = 0;
@@ -1913,42 +1912,63 @@ function getClassedTableHtml(table:string = '') {
     let textOut = textIn;
     // textOut = formatAsString(textIn);
 
-
-    // COPY to tableStylesClipboard
+    let tableStylesClipboard:any[] = [];
 
     // find tables
     let tableMatches = [...textOut.matchAll(/<table[\s\S]*?<\/table>/gim)];
     numFound = tableMatches.length;
+    if (numFound === 0) {
+      vscode.window.showInformationMessage('Error: No tables found in the current selection or document. [copyTableStyles]');
+      return;
+    }
     faodebug.appendLine(tableMatches.length + ' tables found');
-    tableStylesClipboard = [];
-    // find col, tr, th/td
-    // find class, style, rowspan, colspan, scope
 
+    tableMatches.forEach(tableMatch => {
+      let tableString:string = tableMatch[0];
+      let tableStyles:any = {
+        'col': [],
+        'row': [],
+        'cell': [],
+      };
+      // <col...>, <colgroup...>, </colgroup...>
+      let colMatches = [...tableString.matchAll(/<\/?col[^>]*>/gim)];
+      colMatches.forEach(match => {
+        tableStyles.col.push(match[0]);
+      });
+      // <tr...>
+      let rowMatches = [...tableString.matchAll(/<tr[^>]*>/gim)];
+      rowMatches.forEach(match => {
+        tableStyles.row.push(match[0]);
+      });
+      // <th...>, <td...>
+      let cellMatches = [...tableString.matchAll(/<t[dh][^>]*>/gim)];
+      cellMatches.forEach(match => {
+        tableStyles.cell.push(match[0]);
+      });
+      // THEAD, TBODY, TFOOT ?
+      // save this table to clipboard
+      tableStylesClipboard.push(tableStyles);
+    });
 
+    // store table styles in extension global state
+    context.globalState.update('tableStylesClipboard', tableStylesClipboard);
 
-    // PASTE from tableStylesClipboard
-
-    // find tables - same number of tables as in tableStylesClipboard? if not, prompt user to confirm
-    // find col, tr, th/td - same number of col, tr, th/td as in tableStylesClipboard? if not, prompt user to confirm
-    // replace class, style, rowspan, colspan, scope
-
-
-
-    // Finally, replace text
-    replaceCurrentSelectionOrDocumentText(textOut);
     // result message for user
     consoleLog(`copyTableStyles: ${numFound} found, ${numChanged} changed.`);
   });
   context.subscriptions.push(copyTableStyles);
 
-
   const pasteTableStyles = vscode.commands.registerCommand('fao-html-scripts.pasteTableStyles', () => {
     faodebug.appendLine('SCRIPT: pasteTableStyles');
 
-    if (tableStylesClipboard.length < 1) {
+    // get table styles from extension global state
+    let tableStylesClipboard = context.globalState.get<any[]>('tableStylesClipboard');
+    if (!tableStylesClipboard) {
       consoleLog('Error: No table styles found in clipboard. Please run "Copy Table Styles" first. [pasteTableStyles]');
       return;
     }
+    // tableStylesClipboard = [...tableStylesClipboard];
+    faodebug.appendLine(tableStylesClipboard.length + ' tables found in clipboard');
 
     let numFound = 0;
     let numChanged = 0;
@@ -1971,14 +1991,44 @@ function getClassedTableHtml(table:string = '') {
 
 
     // PASTE from tableStylesClipboard
+    // faodebug.appendLine(tableStylesClipboard.toString());
 
-    // find tables - same number of tables as in tableStylesClipboard? if not, prompt user to confirm
+    // find tables
     let tableMatches = [...textOut.matchAll(/<table[\s\S]*?<\/table>/gim)];
     numFound = tableMatches.length;
-    faodebug.appendLine(tableMatches.length + ' tables found');
-    // find col, tr, th/td - same number of col, tr, th/td as in tableStylesClipboard? if not, prompt user to confirm
-    // replace class, style, rowspan, colspan, scope
+    if (numFound === 0) {
+      vscode.window.showInformationMessage('Error: No tables found in the current selection or document. [pasteTableStyles]');
+      return;
+    }
+    faodebug.appendLine(tableMatches.length + ' tables found in document');
 
+    tableMatches.forEach( (tableMatch, i) => {
+      let tableString:string = tableMatch[0];
+      let tableStyles = tableStylesClipboard[i];
+      faodebug.appendLine(`Table ${i}`);
+
+      // replace whole col block
+      let copiedColBlock = tableStyles.col.join('\n');
+      tableString = tableString.replaceAll(/<\/?col[^>]*>[\s\S]*<\/?col[^>]*>/gim,copiedColBlock);
+
+      // replace each row
+      let copiedRows = [...tableStyles.row];
+      let rowMatches = [...tableString.matchAll(/<tr[^>]*>/gim)];
+      faodebug.appendLine(`-> ${rowMatches.length} rows found in table, ${copiedRows.length} rows found in clipboard`);
+      tableString = tableString.replaceAll(/<tr[^>]*>/gim, function(){ return copiedRows.shift(); });
+      
+      //replace each cell opening tag, leave content, parse tag name and match closing tag
+      let copiedCells = [...tableStyles.cell];
+      let cellMatches = [...tableString.matchAll(/<t[dh]([^>]*)>/gim)];
+      faodebug.appendLine(`-> ${cellMatches.length} rows found in table, ${copiedCells.length} cells found in clipboard`);
+      tableString = tableString.replaceAll(/<t[dh]([^>]*)>/gim, function(){ 
+        return copiedCells.shift(); 
+      });
+      tableString = tableString.replaceAll(/<(t[dh])([\s\S]*)<\/(t[dh])>/gim,'<$1$2</$1>');
+
+      // replace table
+      textOut = textOut.replaceAll(tableMatch[0],tableString);
+    });
 
 
     // Finally, replace text
